77edc9c923f1595ed37724b5f86b495171850929
missing114 <haotianpanzju@gmail.com>
2014-03-19 00:33:57 -0400

    change in sup_model

--- sup_model.py
+++ sup_model.py
@@ -9,12 +9,14 @@
 # Note: feature vector is chosen after lemmatizing
 
 from collections import Counter
+from collections import defaultdict
 from sup_preprocessing import *
 
 class featureModel:
 	def __init__(self, trainFileName, windowsize):
-                self.count = {}
-                self.countFea = {}
+                self.countWord = {}
+                self.countWordID = defaultdict(dict)
+                self.countFea = defaultdict(lambda: defaultdict(dict))
                 self.traindataSize = 0
                 self.windowsize = windowsize
                 self.traindata = parse_supervised(trainFileName, windowsize)
@@ -25,20 +27,27 @@ class featureModel:
                         word = word_n_ID[0]
                         ID = word_n_ID[1]
                         for example in examples:
-                                if (word, ID) in self.count:
-                                        self.count[(word, ID)] += 1
+                                if word in self.countWord:
+                                        self.countWord[word] += 1
                                 else:
-                                        self.count[(word, ID)] = 1
+                                        self.countWord[word] = 1
+                                if (word, ID) in self.countWordID:
+                                        self.countWordID[word][ID] += 1
+                                else:
+                                        self.countWordID[word][ID] = 1
                                 for featureword in example.split():
                                         if (word, ID, featureword) in self.countFea:
-                                                self.countFea[(word, ID, featureword)] += 1
+                                                self.countFea[word][ID][featureword] += 1
                                         else:
-                                                self.countFea[(word, ID, featureword)] = 1
+                                                self.countFea[word][ID][featureword] = 1
+##                print self.countWord
+##                print self.countWordID
+##                print self.countFea
                 
         def probSense(self, targetword, targetID):
                 # prob P(s) = # of (word A, sense S) / # of (word A)
-		number1 = 0
-		number2 = 0
+		number1 = self.countWord[targetword]
+		number2 = self.countWordID[targetword][targetID]
 
                 # TODO:
                 # a.
@@ -51,15 +60,9 @@ class featureModel:
                 #       number1 += cnt
                 # b.
                 # Raise an exception instead of Printing an Error
-
-		for (word, ID) in self.count:
-                        if word == targetword:
-                                number1 += self.count[(word, ID)]
-                                if ID == targetID:
-                                        number2 = self.count[(word, ID)]
+                
                 if number1 == 0:
-                        print 'Error: No such word in the training data'
-                        return
+                        raise ZeroDivisionError
                 prob = 1.0 * number2/number1
                 return prob
 
@@ -73,10 +76,13 @@ class featureModel:
                 # if self.countFea[(targetword, targetID, targetfea)] == 0:
                         # print 'Error: No such context in the training data'
                         # return
+
+                number1 = self.countWordID[targetword][targetID]
+		number2 = self.countFea[targetword][targetID][targetfea]
                 if (targetword, targetID, targetfea) not in self.countFea.keys():
                         prob = 0
                 else:
-		      prob = 1.0 * self.countFea[(targetword, targetID, targetfea)]/self.count[targetword, targetID]
+                        prob = 1.0 * number2/number1
                 return prob
 
         
